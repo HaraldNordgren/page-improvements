081680795400168db66dfe2f80aea43bd87bffa6
HaraldNordgren <haraldnordgren@gmail.com>
2017-06-11 15:19:01 +0200

    [Nordnet] - Change mux-page to take 'autospar' into account

--- nordnet/nordnet.js
+++ nordnet/nordnet.js
@@ -1,5 +1,6 @@
-var info = document.getElementsByClassName("info")[1];
+var info_boxes = document.getElementsByClassName("info");
 
+var info = info_boxes[1];
 var currency = info.
 	getElementsByTagName("table")[0].
 	getElementsByTagName("tr")[1].
@@ -7,20 +8,29 @@ var currency = info.
 	innerHTML;
 var total_amount = Number(currency.replace(/[^0-9\.]+/g,"")) / 100;
 
-var funds = info.
-	getElementsByTagName("table")[1].
-	getElementsByTagName("tbody")[0].
-	getElementsByTagName("tr")
+function append_to_box(info) {
+	var tables = info.getElementsByTagName("table")
+	if (tables.length == 0) {
+		return;
+	}
 
-for (var i=1; i<funds.length; i++) {
-	var fund = funds[i];
-	var tds = fund.getElementsByTagName("td");
-	var percentage = parseFloat(tds[1].innerHTML) / 100;
+	var funds = tables[1].
+		getElementsByTagName("tbody")[0].
+		getElementsByTagName("tr")
 
-	var node = document.createElement("td");
-	var textnode = document.createTextNode(
-		(total_amount * percentage).toString() + " SEK");
-	node.appendChild(textnode);
-	
-	fund.insertBefore(node, tds[1])
+	for (var i=1; i<funds.length; i++) {
+		var fund = funds[i];
+		var tds = fund.getElementsByTagName("td");
+		var percentage = parseFloat(tds[1].innerHTML) / 100;
+
+		var node = document.createElement("td");
+		var textnode = document.createTextNode(
+			Math.round(total_amount * percentage).toString() + " SEK");
+		node.appendChild(textnode);
+		
+		fund.insertBefore(node, tds[1])
+	}
 }
+
+//append_to_box(info_boxes[0]);
+append_to_box(info_boxes[2]);
