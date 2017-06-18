88337fb8c86b0027b28d6ab16ce153d9888c5354
HaraldNordgren <haraldnordgren@gmail.com>
2017-06-18 19:50:24 +0200

    [Avanza] - Use real percentages to  calculate Månadsspar diff on Overview page

--- avanza/funds.js
+++ avanza/funds.js
@@ -1,23 +1,46 @@
+map = {}
+
+$.ajax({
+    url: "https://www.avanza.se/mina-sidor/manadsspara.html",
+    async: false,
+    success: function(data1) {
+        
+        parsed_data = $(new DOMParser().parseFromString(data1, 'text/html'));
+        //extratorrent_domain = "https://www.avanza.se";
+        //$('head', parsed_data).append('<base href="' + extratorrent_domain + '">');
+
+        //source = document
+        source = parsed_data[0]
+
+        rows = source.getElementsByClassName("fundRow")
+        for (i=1; i<rows.length; i++) {
+        	tds = rows[i].
+        		getElementsByTagName("td")
+
+        	name = tds[0].
+        		getElementsByTagName("a")[0].
+        		innerHTML
+        	percentage = parseFloat(tds[2].innerHTML.trim())
+        	map[name] = percentage
+        }
+}})
+//console.log(map)
+
 function getPart(el, index) {
 	return parseInt(el.
 		getElementsByClassName("tRight")[index].
 		innerHTML.
 		replace(/&nbsp;/g,''))}
 
-nonMontly = [
-	"Evli Global B",
-	"SPP Aktiefond Global"
-]
+function appendTd(s, prevNode, index) {
+	var node = document.createElement("td");
+	node.appendChild(document.createTextNode(s));
+	tr = prevNode.getElementsByClassName("tRight")[index]
+	prevNode.insertBefore(node, tr)
+}
 
 table = document.getElementsByClassName("positions")[0].
 	getElementsByTagName("table")[1]
-
-/*
-total = getPart(table.
-	getElementsByTagName("tfoot")[0].
-	getElementsByTagName("tr")[0], 1)
-console.log("Total: " + total)
-*/
 
 trs = table.
 	getElementsByTagName("tbody")[0].
@@ -27,7 +50,9 @@ total = 0
 for (i=0; i<trs.length; i++) {
 	title = trs[i].getElementsByClassName("instrumentName")[0].
 		getElementsByTagName("a")[0].title
-	if (nonMontly.indexOf(title) != -1) {
+	//console.log(title)
+	//console.log(title in map)
+	if (!(title in map)) {
 		continue
 	}
 
@@ -38,17 +63,40 @@ for (i=0; i<trs.length; i++) {
 for (i=0; i<trs.length; i++) {
 	title = trs[i].getElementsByClassName("instrumentName")[0].
 		getElementsByTagName("a")[0].title
-	if (nonMontly.indexOf(title) != -1) {
-		continue
+	
+	if (title in map) {
+		partSum = getPart(trs[i], 4)
+		percentage = parseFloat(partSum) / total * 100
+		
+		percentageDiff = percentage - map[title]
+		//s = percentage.toFixed(1) + "% (" + percentageDiff.toFixed(1) + "%)"
+		if(percentageDiff > 0){
+        	sign = "+"
+    	} else {
+    		sign = ""
+    	}
+		s = sign + percentageDiff.toFixed(1) + "%"
+
+	
+	} else {
+		s = ""
 	}
 
-	partSum = getPart(trs[i], 4)
-	var percentage = parseFloat(partSum) / total * 100;
-	s = percentage.toFixed(1).toString() + "%"
+	appendTd(s, trs[i], 4)
 
+
+	/*
 	var node = document.createElement("td");
-	var textnode = document.createTextNode(s);
-	node.appendChild(textnode);
+	node.appendChild(document.createTextNode(s));
 	tr = trs[i].getElementsByClassName("tRight")[4]
 	trs[i].insertBefore(node, tr)
+	*/
 }
+
+appendTd("Månadsspar", table.
+	getElementsByTagName("thead")[0].
+	getElementsByTagName("tr")[0], 4)
+
+appendTd("", table.
+	getElementsByTagName("tfoot")[0].
+	getElementsByTagName("tr")[0], 1)
